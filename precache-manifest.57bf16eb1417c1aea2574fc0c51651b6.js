68a29b3f863e561edf3424d1f90e409aba85310b
rizwanishaq <rizwanishaq@gmail.com>
2019-12-26 17:37:22 +0100

    Updates

---
+++ precache-manifest.57bf16eb1417c1aea2574fc0c51651b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3691538fc97a642b771ab9e49e3c862b",
+    "url": "/pokemone/index.html"
+  },
+  {
+    "revision": "fb0a9ff30b3685df0c6a",
+    "url": "/pokemone/static/css/main.ebecfd4f.chunk.css"
+  },
+  {
+    "revision": "3ccd210a5fe7ad0e9f4a",
+    "url": "/pokemone/static/js/2.cc13bd5c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokemone/static/js/2.cc13bd5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb0a9ff30b3685df0c6a",
+    "url": "/pokemone/static/js/main.917fe096.chunk.js"
+  },
+  {
+    "revision": "79964351decddeec5032",
+    "url": "/pokemone/static/js/runtime-main.6beacac2.js"
+  }
+]);
